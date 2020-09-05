75950ddfbf0b16927464793dd30d3da1d95506c8
uimarshall <uimarshall@gmail.com>
2020-09-05 15:04:23 +0100

    Add comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles #This told Rails that we were going to follow the RESTful conventions for this model named Article. 
+  resources :articles  do #This told Rails that we were going to follow the RESTful conventions for this model named Article. 
+    resources :comments
+  end
 end
